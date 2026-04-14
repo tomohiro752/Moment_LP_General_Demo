e46c5036f7192bc13f9f36812294eb2b61de6a64
tomohiro752 <tomo8.h.c@gmail.com>
2026-04-15 01:01:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,6 +12,32 @@ $(function () {
     $("header").toggleClass("open");
   });
 
+
+/*=================================================
+  カード scale
+  ===================================================*/
+
+  $(window).on('scroll', function(){
+
+    $('.fade-up').each(function () {
+      const elemPos = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+
+      if(scroll > elemPos - windowHeight + 100) {
+        $(this).addClass('is-show');
+      }
+    });
+
+  });
+
+
+
+
+
+
+
+
 })
 
 
